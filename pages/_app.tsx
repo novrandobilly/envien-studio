4a23aea9eda68006c662df26ed7f50778051b989
novrandobilly <novrandobilly@gmail.com>
2022-01-27 19:17:51 +0700

    Refine extra wide screen layout and install google analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <script async src='https://www.googletagmanager.com/gtag/js?id=UA-174977000-1'></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'UA-174977000-1');
+                `,
+          }}></script>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
